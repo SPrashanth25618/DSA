aa5cf1c4afba828ab3d9a06e4d224290ea3c0df4
Prashanth <mail2sprashanth@gmail.com>
2025-04-11 15:43:58 +0530

    add optimal solution to the missing_number problem

--- Missing_Number.cpp
+++ Missing_Number.cpp
@@ -22,3 +22,22 @@ class Solution
 };
 // T.C = O(N^2)
 // S.C = O(1)
+
+//Optimal
+//Optimal
+class Solution 
+{
+    public:
+        int missingNumber(vector<int>& nums) 
+        {
+            int n = nums.size();
+            int x = 0;
+            for(int i=0;i<n;i++){
+                x ^= i;
+                x ^= nums[i];
+            }
+            return x^n;
+        }
+};
+// T.C = O(N)
+// S.C = O(1)
