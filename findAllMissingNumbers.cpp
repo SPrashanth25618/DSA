aeaac9086ae9455e2ed823fecc298442550cee68
SPrashanth25618 <mail2sprashanth@gmail.com>
2025-04-25 12:43:48 +0530

    added solution 3

--- findAllMissingNumbers.cpp
+++ findAllMissingNumbers.cpp
@@ -52,3 +52,18 @@ public:
         return ans;
     }
 };
+
+class Solution
+{
+public:
+    vector<int> findDisappearedNumbers(vector<int> &nums)
+    {
+        unordered_set<int> s(nums.begin(),nums.end());
+        vector<int> ans;
+        int n=nums.size();
+        for(int i=1;i<=n;i++)
+            if(!s.count(i))
+                ans.push_back(i);
+        return ans;
+    }
+};
