06b7191b77b6cfba807aa9ddc67699b7ebc1cbd1
SPrashanth25618 <mail2sprashanth@gmail.com>
2025-04-25 12:50:50 +0530

    added solution 1

--- smallerNumbers.cpp
+++ smallerNumbers.cpp
@@ -16,3 +16,32 @@ public:
         return ans;
     }
 };
+
+class Solution
+{
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int> &nums)
+    {
+        vector<int> cnt(101, 0);
+        int n = nums.size();
+        vector<int> ans(n, 0);
+        for (int i = 0; i < n; i++)
+        {
+            cnt[nums[i]]++;
+        }
+        for (int i = 1; i < 100; i++)
+        {
+            cnt[i] = cnt[i] + cnt[i - 1];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] == 0)
+            {
+                ans[i] = 0;
+            }
+            else
+                ans[i] = cnt[nums[i] - 1];
+        }
+        return ans;
+    }
+};
