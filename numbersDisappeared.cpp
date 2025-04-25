4c88434ec1b19d4eb8a0cbbfb84d0ef7915a0a63
SPrashanth25618 <mail2sprashanth@gmail.com>
2025-04-25 12:56:10 +0530

    added solution 3

--- numbersDisappeared.cpp
+++ numbersDisappeared.cpp
@@ -42,14 +42,29 @@ public:
     vector<int> findDisappearedNumbers(vector<int> &nums)
     {
         // solution -2;
-        int n=nums.size();
+        int n = nums.size();
         vector<int> ans;
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<=n;i++)
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i <= n; i++)
         {
-            if(!bssearch(nums,i))
+            if (!bssearch(nums, i))
                 ans.push_back(i);
         }
+        return ans;
+    }
+};
+
+class Solution
+{
+public:
+    vector<int> findDisappearedNumbers(vector<int> &nums)
+    {
+        unordered_set<int> s(nums.begin(),nums.end());
+        vector<int> ans;
+        int n=nums.size();
+        for(int i=1;i<=n;i++)
+            if(!s.count(i))
+                ans.push_back(i);
         return ans;
     }
 };
