a6f51b5fe9a0cca65190edf90a4e66cf98e3472b
SPrashanth25618 <mail2sprashanth@gmail.com>
2025-04-25 12:54:29 +0530

    added solution 1

---
+++ numbersDisappeared.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    vector<int> findDisappearedNumbers(vector<int> &nums)
+    {
+        int n=nums.size();
+        vector<int> ans;
+        for(int i=1;i<=n;i++)
+        {
+            bool flag = true;
+            for(int j:nums)
+                if(j==i)
+                {
+                    flag=false;
+                    break;
+                }
+            if(flag)
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
